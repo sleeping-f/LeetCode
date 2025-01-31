883458fd697fb2f162494e71da76938d1b3752a9
sleeping-f <102950795+sleeping-f@users.noreply.github.com>
2025-01-31 20:17:58 +0600

    Time: 0 ms (100%), Space: 13.4 MB (25.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,8 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        val_idx = {}
+        n = len(nums)
+        for i in range(n):
+            if target - nums[i] in val_idx:
+                return [val_idx[target-nums[i]], i]
+            val_idx[nums[i]] = i
+
+
+"""First Solution: time o(n^2)
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-        
+"""
