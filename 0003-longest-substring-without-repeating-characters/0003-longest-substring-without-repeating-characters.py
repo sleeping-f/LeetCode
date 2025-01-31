76a315a14e4940d52a002cd90c5d0b4f914a064d
sleeping-f <102950795+sleeping-f@users.noreply.github.com>
2025-01-31 18:02:26 +0600

    Time: 442 ms (14.11%), Space: 12.5 MB (79.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,18 +4,38 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        subs = ''
+        l, r, ans = 0, 0, 0
+        ls = len(s)
+        window = ''
+
+        while r < ls:
+            while  r < ls and (not s[r] in window):
+                window += s[r]
+                r += 1
+            ans = max(ans, r - l)
+
+            while  r < ls and s[l] != s[r] and l < r:
+                l += 1
+                window = window[1:]
+            window = window[1:]
+            l += 1
+            print('3- ', window)
+
+        return ans 
+
+
+""" First Solve: time o(n^2)
         max = 0
-        count = 0
-        for ch in s:
-            if not ch in subs:
-                subs += ch
-                count += 1
-            else:
-                subs = ch
-                if count > max: max = count
-                count = 1
-        if count > max: max = count
+        for r in range(len(s)):
+            subs = s[r]
+            count = 1
+            for i in range(r+1, len(s)):
+                if not s[i] in subs:
+                    subs += s[i]
+                    count += 1
+                else:
+                    break
+            if max < count: max = count
 
         return max
-            
+            """
