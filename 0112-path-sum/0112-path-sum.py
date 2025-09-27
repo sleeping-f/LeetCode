ac03875bfdcc669372006df15971968f2e85d3d1
Md. Farhan Sadique <102950795+sleeping-f@users.noreply.github.com>
2025-09-27 13:04:57 +0600

    Time: 7 ms (11.35%), Space: 14.3 MB (62.94%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: bool
+        """
+        if root == None: return False
+
+        from collections import deque
+
+        q = deque()
+
+        q.append([root, root.val])
+
+        while q:
+            rt, total = q.popleft()
+
+            if rt.left == None and rt.right == None:
+                if total == targetSum: return True
+            elif rt.left and not rt.right:
+                q.append([rt.left, total + rt.left.val])
+            elif rt.right and not rt.left:
+                q.append([rt.right, total + rt.right.val])
+            else:
+                q.append([rt.left, total + rt.left.val])
+                q.append([rt.right, total + rt.right.val])
+
+        return False
